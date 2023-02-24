f294e16cca31a91953c1014dd76ea2340d12fa0a
Tobias Weiss <tobias@tobias-weiss.org>
2023-02-24 15:44:45 +0100

    add first samples

---
+++ EventSource.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventSource : MonoBehaviour
+{
+
+    public event EventHandler OnSpacePressed;
+
+
+    public class OnSpacePressedEventArgs : EventArgs
+    {
+        public int spaceCount;
+    }
+
+
+    public event EventHandler<OnSpacePressedEventArgs> OnSpacePressedWithArgs;
+    private int spaceCount;
+
+    public delegate void FloatEvent(float f);
+    public event FloatEvent OnFloatEvent;
+
+    public event Action<bool, int> OnActionEvent;
+
+    public UnityEvent OnUnityEvent;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke(this, EventArgs.Empty);
+
+            spaceCount++;
+            OnSpacePressedWithArgs?.Invoke(this, new OnSpacePressedEventArgs { spaceCount = spaceCount });
+
+            OnFloatEvent?.Invoke(5.5f);
+
+            OnActionEvent?.Invoke(true, 123);
+
+            OnUnityEvent?.Invoke();
+        }
+
+    }
+}
