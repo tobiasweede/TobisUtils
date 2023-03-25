5b3a7ce35f75a4d294d28194a3080b843ed489ab
Tobias Weiss <tobias@tobias-weiss.org>
2023-03-25 16:07:13 +0100

    add math

--- TobisUtils.cs
+++ TobisUtils.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 namespace TobisUtils
 {
+    public static class Math {
+        public const float TAU = 6.283185307179586f;
+        public static Vector2 AngToDir2D( float angRad ) => new Vector2 ( Mathf.Cos ( angRad ), Mathf.Sin( angRad ));
+        public static float DirToAng2D( Vector2 v ) => Mathf.Atan2(v.y, v.x);
+    }
     public static class LoadData
     {
         public static Texture2D ImageAsset(string path)
