9838ec5353114af3a11a6b4d352dcd4932dab2cd
Tobias Weiss <tobias@tobias-weiss.org>
2023-11-25 14:17:06 +0100

    w

---
+++ Logging.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Logging : MonoBehaviour
+{
+    public static int logSize = 3;
+    public string folder = "/Recordings/";
+    public string logname = "dummy";
+    private bool logging = false;
+    private string[] logData = new string[logSize];
+    prinvate logPath = "";
+    private StreamWriter writer = null;
+    private static readonly string[] columnNames = {
+        "headerText",
+        "modalText",
+        "rating",
+    };
+    private int flushCounter = 0;
+    void Awake()
+    {
+        // create filename with timestamp
+        DateTime now = DateTime.Now;
+        string fileName = string.Format("{0}-{1:00}-{2:00}-{3:00}h{4:00}m-{5}-" + logname, now.Year, now.Month, now.Day, now.Hour, now.Minute, SceneManager.GetActiveScene().name);
+        string logPath = Application.persistentDataPath + folder;
+        if (!Directory.Exists(logPath)) Directory.CreateDirectory(logPath);
+        string path = logPath + fileName + ".csv";
+
+        StartLogging();
+    }
+
+    private void OnDestroy()
+    {
+        StopLogging();
+    }
+    public void StartLogging()
+    {
+        if (logging)
+        {
+            Debug.LogWarning("Logging was on when StartLogging was called. No new log was started.");
+            return;
+        }
+        logging = true;
+        writer = new StreamWriter(path);
+
+        Log(columnNames);
+        Debug.Log("Log file started at: " + path);
+    }
+
+    private void StopLogging()
+    {
+        if (!logging)
+            return;
+
+        if (writer != null)
+        {
+            writer.Flush();
+            writer.Close();
+            writer = null;
+        }
+        logging = false;
+        Debug.Log("Logging ended at: " + path);
+    }
+
+    private void Log(string[] values)
+    {
+        if (!logging || writer == null)
+            return;
+
+        string line = "";
+        for (int i = 0; i < values.Length; ++i)
+        {
+            values[i] = values[i].Replace("\r", "").Replace("\n", ""); // Remove new lines so they don't break csv
+            line += values[i] + (i == (values.Length - 1) ? "" : ";"); // Do not add semicolon to last data string
+        }
+        writer.WriteLine(line);
+
+        flushCounter += 1;
+        if (flushCounter > 10)
+        {
+            writer.FlushAsync();
+            flushCounter = 0;
+        }
+    }
+
+
+}
